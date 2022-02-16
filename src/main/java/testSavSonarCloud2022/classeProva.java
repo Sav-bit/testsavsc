a4ab0ab2d2465361d41b848578b966fed303ae2b
Sav <56634996+Sav-bit@users.noreply.github.com>
2022-02-16 07:41:04 +0100

    first commit

---
+++ src/main/java/testSavSonarCloud2022/classeProva.java
@@ -0,0 +1,10 @@
+package testSavSonarCloud2022;
+
+public class classeProva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ciao!");
+	}
+
+}
